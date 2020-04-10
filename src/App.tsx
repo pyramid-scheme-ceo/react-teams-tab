94c8276ee35f96d1984f9b53e57eac43d1e29af8
Matt A <matt.astach@gmail.com>
2020-04-10 15:02:03 +0930

    Add msteams library and call to configure save

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { RouterHelper } from './helpers';
+import * as msTeams from '@microsoft/teams-js';
 
 function App() {
+  msTeams.initialize();
+  
   return RouterHelper.RenderRoutes();
 }
 
--- src/pages/configure.tsx
+++ src/pages/configure.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import * as msTeams from '@microsoft/teams-js';
 
 export default function Configure() {
+  msTeams.settings.registerOnSaveHandler(saveEvent => {
+    msTeams.settings.setSettings({
+      contentUrl: window.location.origin,
+      entityId: window.location.origin
+    });
+
+    saveEvent.notifySuccess();
+  });
+  msTeams.settings.setValidityState(true);
+
   return (
     <h1>Configure</h1>
   );
