2f759f3309308028408317ca4598c4b434caaf75
Matt A <matt.astach@gmail.com>
2020-05-09 13:00:30 +0930

    Add comments to auth helper

--- src/helpers/auth-helper.ts
+++ src/helpers/auth-helper.ts
@@ -11,6 +11,11 @@ const authenticationContext = new AuthenticationContext({
 });
 
 export default class AuthHelper {
+    /**
+     * Uses the current authetication context to check if a user
+     * is logged in. In this case, this is determined by the presence
+     * of a cached user and cached token with length > 0.
+     */
     public static IsUserLoggedIn(): boolean {
         let cachedUser = authenticationContext.getCachedUser();
         let cachedToken = authenticationContext.getCachedToken(constants.Auth.appId);
@@ -18,13 +23,16 @@ export default class AuthHelper {
         return !!cachedUser && cachedToken.length > 0;
     }
 
+    /**
+     * Attempts to get an access token for the user. If successful,
+     * sends the user to the home page again where they will now
+     * be logged in.
+     */
     public static async Login(): Promise<void> {
         let accessToken: string = '';
 
         try {
             accessToken = await AuthHelper.getAccessToken();
-            console.log("Got token")
-            console.log(accessToken);
         } catch (err) {
             console.error(err)
         }
@@ -34,11 +42,20 @@ export default class AuthHelper {
         }
     }
 
+    /**
+     * Clears any existing user from the cache, then requests
+     * an AD token.
+     */
     public static StartSignIn(): void {
         authenticationContext.clearCache();
         authenticationContext.login();
     }
 
+    /**
+     * Called from the sign-in-end page. Checks for the presence
+     * of the AD token, and notifies teams of a successful log in
+     * if it is there, or notifies of failure otherwise.
+     */
     public static EndSignIn(): void {
         if (authenticationContext.isCallback(window.location.hash)) {
             authenticationContext.handleWindowCallback(window.location.hash);
@@ -61,6 +78,10 @@ export default class AuthHelper {
         }
     }
 
+    /**
+     * Begins the login flow by opening a popup window
+     * at the sign-in-start page.
+     */
     private static async getAccessToken(): Promise<string> {
         return new Promise<string>((resolve, reject) => {
             msTeams.authentication.authenticate({
